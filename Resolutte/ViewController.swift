75c20760c44aba106a225e7a2e17e3da26db9cd0
Harry C <38871934+HryChg@users.noreply.github.com>
2021-01-09 14:51:39 -0800

    Initial Commit

---
+++ Resolutte/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Resolutte
+//
+//  Created by Harry Chuang on 1/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
