b26f4fddeb6d9ad2c628219eaaa93211d712e673
Harry C <38871934+HryChg@users.noreply.github.com>
2021-01-09 15:12:01 -0800

    Starter Project

--- Resolutte/ViewController.swift
+++ Resolutte/ViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        // Test Sign In
+        Auth.auth().signIn(withEmail: "test@email.com", password: "123") { [weak self] authResult, error in
+          guard let strongSelf = self else { return }
+          print("Sign in Success.")
+        }
     }
 
 
